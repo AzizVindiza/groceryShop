a5d2a6b97d6550c66611980ed194537eea38c590
shikaridoki <rasul.vindiza@gmail.com>
2023-02-07 20:06:08 +0600

    prwe

--- src/Components/StarRating.js
+++ src/Components/StarRating.js
@@ -1,33 +1,33 @@
-// import React, { useState } from "react";
-// // import { FaStar } from "react-icons/fa"
-//
-//
-// const StarRating = () => {
-//     const [rating, setRating] = useState(null);
-//     const [hover, setHover] = useState(null);
-//     return <div className="star-rating">
-//         {[...Array(5)].map((star, i) => {
-//             const ratingValue = i +1;
-//             return (
-//                 <label>
-//                     <input
-//                     className="star-rating__input"
-//                         type="radio"
-//                         name="rating"
-//                         value={ratingValue}
-//                         onClick={() => setRating(ratingValue)}
-//                         />
-//                     <FaStar
-//                     className="star-rating__item"
-//                     color={ratingValue <= (hover || rating) ? "#FF6633" : "#BFBFBF"}
-//                     onMouseEnter={() => setHover(ratingValue)}
-//                     onMouseLeave={() => setHover(null)}
-//                     />
-//                 </label>
-//             )
-//         })}
-//
-//     </div>
-// }
-//
-// export default StarRating
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa"
+
+
+const StarRating = () => {
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+    return <div className="star-rating">
+        {[...Array(5)].map((star, i) => {
+            const ratingValue = i +1;
+            return (
+                <label>
+                    <input
+                    className="star-rating__input"
+                        type="radio"
+                        name="rating"
+                        value={ratingValue}
+                        onClick={() => setRating(ratingValue)}
+                        />
+                    <FaStar
+                    className="star-rating__item"
+                    color={ratingValue <= (hover || rating) ? "#FF6633" : "#BFBFBF"}
+                    onMouseEnter={() => setHover(ratingValue)}
+                    onMouseLeave={() => setHover(null)}
+                    />
+                </label>
+            )
+        })}
+
+    </div>
+}
+
+export default StarRating
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -4,14 +4,18 @@ import ProductTitle from "./ProductTitle/ProductTitle";
 import ProductSame from "./ProductSame/ProductSame";
 import ProductReviews from "./ProductReviews/ProductReviews";
 import ProductDiscount from "./ProductDiscount/ProductDiscount";
+import HeartFavorite from "../../Components/HeartFavorite";
 
 const Product = () => {
     return (
         <>
+
+
             <ProductTitle/>
             <ProductSame/>
             <ProductReviews/>
             <ProductDiscount/>
+
         </>
     );
 };
