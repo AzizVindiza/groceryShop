18026adcaee32ec02bbe0f47057535cba74b3924
shikaridoki <rasul.vindiza@gmail.com>
2023-02-18 18:25:43 +0600

    as

--- src/pages/Product/ProductSame/ProductSame.jsx
+++ src/pages/Product/ProductSame/ProductSame.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./productSame.scss"
 import Title from "../ComponentsProduct/Title/Title";
 import halal from "../ComponentsProduct/ProductImage/sausage.png"
-import ProductCart from "./ProductCart/ProductCart";
+import ProductCard from "./ProductCart/ProductCard";
 
 
 
@@ -12,7 +12,7 @@ const ProductSame = () => {
             <div className="container">
                 <Title name="С этим товаров покупают"/>
                 <div className="wrapper">
-                    <ProductCart/>
+                    <ProductCard/>
 
                 </div>
             </div>
